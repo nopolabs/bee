a0505c4bc59f84f4ff5ab94af49833a4e3377f70
Dan Revel <dan@nopolabs.com>
2021-09-22 16:56:23 -0700

    pangrams

--- src/main/java/com/nopolabs/bee/ToBee.java
+++ src/main/java/com/nopolabs/bee/ToBee.java
@@ -1,26 +1,45 @@
 package com.nopolabs.bee;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ToBee {
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws IOException {
 
         List<String> wordList = WordSource.get(WordSource.DICT_WORDS_FILE).collect(Collectors.toList());
 
-        List<String> pangrams = pangrams(wordList.stream()).collect(Collectors.toList());
+        AtomicInteger count = new AtomicInteger();
 
-        pangrams.stream()
+        Stream<String> stream = pangrams(wordList.stream())
                 .flatMap(ToBee::permute)
-                .forEach(pangram -> {
+                .distinct()
+                .map(pangram -> {
+                    System.out.println(count.incrementAndGet());
                     final Search search = new Search(wordList.stream(), pangram);
                     final Words words = search.run();
-                    System.out.printf("%s found %d words worth %d points%n", pangram, words.size(), words.totalScore());
+                    Info info = new Info(pangram, words.size(), words.totalScore());
+                    return String.format("%s %5d %5d%n", info.pangram, info.words, info.points);
                 });
+
+        Files.write(Paths.get("/tmp/pangrams.txt"), (Iterable<String>)stream::iterator);
+    }
+
+    static class Info {
+        final String pangram;
+        final int words;
+        final int points;
+
+        Info(String pangram, int words, int points) {
+            this.pangram = pangram;
+            this.words = words;
+            this.points = points;
+        }
     }
 
     private static Stream<String> permute(String p) {
@@ -35,7 +54,7 @@ public class ToBee {
         );
     }
 
-    private static Stream<String> pangrams(Stream<String> words) throws URISyntaxException, IOException {
+    private static Stream<String> pangrams(Stream<String> words) {
         return words
                 .filter(w -> w.length() >= 7)
                 .map(String::toLowerCase)
